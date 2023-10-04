c6427970533fe946fef7340ae3c0eb77191b9afe
Austin Berry <aberry52098@gmail.com>
2023-10-04 14:05:08 -0400

    default home page route

---
+++ src/routes/home.jsx
@@ -0,0 +1,9 @@
+export default function Home (){
+
+    return (
+        <>
+        <p>Jeff does not approve</p>
+        <img className="jeff" src="/public/images/jeff.jpg"/>
+        </>
+    )
+}
