c69d5c094ecd0e2197756bd48e79dd50523a9b1e
Austin Berry <aberry52098@gmail.com>
2023-10-04 13:59:28 -0400

    add styles and navbar

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -1,10 +1,48 @@
-export function loader(){
-    return "";
+import { useLoaderData, Link, Outlet } from "react-router-dom";
+import styled from "styled-components";
+import { CategoryDropdown } from "../components/CategoryDropdown";
+
+const Nav = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    margin: 30px;
+    .cart {
+        max-width: 50px;
+        height: auto;
+    }
+    .real {
+        font-size: x-small;
+    }
+`
+const RootStyle = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+`
+
+export async function loader() {
+    const url = "https://fakestoreapi.com/products/categories";
+    const categories = await fetch(url).then(response => response.json());
+    console.log(categories);
+    return categories;
 }
 export default function Root() {
+    const categories = useLoaderData();
     return (
         <>
-        <div>Amazoon</div>
+            <Nav>
+                <label>
+                    <Link to={"/"}> <img src="/public/images/logo-transparent.PNG" /></Link>
+                    <p className="real">nothing is real</p>
+                </label>
+
+                <CategoryDropdown categories={categories}/>
+                <Link to={"categories"}><img src="/public/images/cart-icon.png" className="cart" /></Link>
+            </Nav>
+            <RootStyle>
+                <Outlet />
+            </RootStyle>
         </>
     )
 }
