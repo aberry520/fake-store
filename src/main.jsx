336bc95b799d39025d4734601160a3880e3e601c
Austin Berry <aberry52098@gmail.com>
2023-10-04 13:58:10 -0400

    add paths and default home page

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,39 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import Root, {loader as rootLoader} from './routes/root.jsx'
+import Root, { loader as rootLoader } from './routes/root.jsx'
+import Category, { loader as categoryLoader } from './routes/category'
+import Categories from './routes/categories'
+import ErrorPage from './routes/error-page'
+import Home from './routes/home'
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Root/>,
+    element: <Root />,
     loader: rootLoader,
-  }
+    children: [{
+      errorElement: <ErrorPage />,
+      children: [
+        {
+          index: true, 
+          element: <Home />
+
+        },
+        {
+          path: "categories/",
+          element: <Categories />,
+          loader: categoryLoader,
+        },
+        {
+          path: "category/:category",
+          element: <Category />,
+          loader: categoryLoader,
+        },
+      ]
+    }
+    ],
+  },
 ])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
